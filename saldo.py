4af447e7a52e766cf6ff9035dcac006602f3bc00
Kamil <kamilrusztyn@gmail.com>
2022-01-23 14:16:25 +0100

    sprzedaz i saldo działa

--- saldo.py
+++ saldo.py
@@ -1,1 +1,8 @@
+from settings2 import czytaj_sys, Saldo, zapytanie_log, glowna_petla, wczytywanie_danych
+
+obj = Saldo()
+sciezka, opcje = czytaj_sys()
+towary, obiekty, suma_saldo, operacje_zatwierdzone = glowna_petla(sciezka)
+
+obj.dopisz_log(operacje_zatwierdzone, opcje, suma_saldo, obiekty, sciezka)
 
---
+++ sprzedaz.py
@@ -0,0 +1,3 @@
+from settings2 import zapytanie_sprzedaz
+
+zapytanie_sprzedaz()
